ca1388ded7ccdd11728523c6c7d08e16871f6467
Brayan Gonzalez <brayangon.18@outlook.com>
2026-03-27 13:23:14 -0500

    fix puerto render

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 import os
 from app import create_app
 
-if __name__ == '__main__':
-    env = os.getenv('FLASK_ENV', 'development')
-    app = create_app(env)
-    app.run(debug=True, host='127.0.0.1', port=5000)
+app = create_app()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
